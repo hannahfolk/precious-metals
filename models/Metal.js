24f1c1634601be5321eb082644cf3e13ec695c77
hfolk25 <hfolk25@gmail.com>
2021-02-02 00:48:13 -0800

    adding arrow function

--- models/Metal.js
+++ models/Metal.js
@@ -1,13 +1,12 @@
-module.exports = function(sequelize, DataTypes) {
-    const Metal = sequelize.define("Metal", {
-      createdAt: { type: DataTypes.DATE, primaryKey: true },
-      gold: DataTypes.DECIMAL(10, 4),
-      silver: DataTypes.DECIMAL(10, 4),
-      platinum: DataTypes.DECIMAL(10, 4),
-      palladium: DataTypes.DECIMAL(10, 4),
-      rhodium: DataTypes.DECIMAL(10, 4)
-    });
+module.exports = (sequelize, DataTypes) => {
+  const Metal = sequelize.define("Metal", {
+    createdAt: { type: DataTypes.DATE, primaryKey: true },
+    gold: DataTypes.DECIMAL(10, 4),
+    silver: DataTypes.DECIMAL(10, 4),
+    platinum: DataTypes.DECIMAL(10, 4),
+    palladium: DataTypes.DECIMAL(10, 4),
+    rhodium: DataTypes.DECIMAL(10, 4),
+  });
 
-    return Metal;
-  };
-  
+  return Metal;
+};
